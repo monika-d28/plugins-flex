33298c26d2b46f8ffa479a8bf93596ebfa7685bf
sdasari <sdasari@rbfcu.org>
2024-02-23 10:42:40 -0600

    sample plugins

---
+++ plugin-sample-2/src/components/CustomTaskList/CustomTaskList.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import { Alert } from '@twilio-paste/core/alert';
+import { Theme } from '@twilio-paste/core/theme';
+import { Text } from '@twilio-paste/core/text';
+
+const CustomTaskList = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  if (!isOpen) {
+    return null;
+  }
+
+  const dismiss = () => setIsOpen(false);
+
+  return (
+    <Theme.Provider theme="default">
+      <Alert onDismiss={dismiss} variant="neutral">
+        <Text>
+          This is a dismissible demo component for flex2.0 test on QA version1.0.1.
+        </Text>
+        <Text>
+          Additional text from plugin-sample-2
+        </Text>
+      </Alert>
+    </Theme.Provider>
+  );
+};
+
+export default CustomTaskList;
---
+++ plugin-sample-2/src/index.js
@@ -0,0 +1,5 @@
+import * as FlexPlugin from '@twilio/flex-plugin';
+
+import Sample2Plugin from './Sample2Plugin';
+
+FlexPlugin.loadPlugin(Sample2Plugin);
